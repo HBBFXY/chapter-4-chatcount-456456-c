ebab4b5a266f209f7c2bd5de08a2b7152a48cdf8
456456-c <1753340693@qq.com>
2025-10-14 20:03:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,15 @@ spaces = 0
 others = 0
 
 for ch in s:
-    # 只算英文字母 A-Z, a-z
     if ('A' <= ch <= 'Z') or ('a' <= ch <= 'z'):
         letters += 1
     elif ch.isdigit():
         digits += 1
-    elif ch.isspace():
+    elif ch == ' ':
         spaces += 1
     else:
         others += 1
 
-# 严格匹配测试脚本要求的输出格式（英文冒号+1空格）
 print("英文字符: {}".format(letters))
 print("数字: {}".format(digits))
 print("空格: {}".format(spaces))
